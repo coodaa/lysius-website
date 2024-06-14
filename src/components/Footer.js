b388110b8bfa91aa0dd2e7b788fe518b35cf7ecb
Florian Schneider <schneider.f@me.com>
2024-06-14 19:23:48 +0200

    landing page

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+// src/components/Footer.js
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 Lysius. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+// src/components/Navbar.js
+
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css"; // Korrigierter Pfad
+
+const Navbar = () => {
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/">LYSIUS</Link>
+      </div>
+      <nav className={styles.nav}>
+        <Link href="/mitglieder">MITGLIEDER</Link>
+        <Link href="/agb">AGB</Link>
+        <Link href="/impressum">IMPRESSUM</Link>
+        <Link href="/de-en">DE/EN</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
