e7e14960964747098ac3493dd89c377daf4c0c20
Florian Schneider <schneider.f@me.com>
2024-07-23 13:53:49 +0200

    clean up code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,19 @@
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import { supabase } from "../lib/supabaseClient";
-import styles from "../styles/Home.module.css";
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const [imageUrl, setImageUrl] = useState("");
-
-  useEffect(() => {
-    const fetchImageUrl = async () => {
-      const { data, error } = supabase.storage
-        .from("public-images") // Name des Buckets
-        .getPublicUrl("michael_donath_86.jpg"); // Pfad zum Bild in deinem Bucket
-
-      if (error) {
-        console.error("Error fetching image URL:", error);
-      } else {
-        console.log("Fetched Image URL:", data.publicUrl); // Debugging-Statement
-        setImageUrl(data.publicUrl);
-      }
-    };
-
-    fetchImageUrl();
-  }, []);
+  const imageUrl = 'https://bohupjvskzxpapqonxbc.supabase.co/storage/v1/object/public/public-images/michael_donath_86.jpg';
 
   return (
     <div className={styles.container}>
       <div className={styles.overlayContainer}>
         <div className={styles.imageWrapper}>
-          {imageUrl ? (
-            <Image
-              src={imageUrl}
-              alt="Michael Donath"
-              className={styles.image}
-              layout="fill"
-            />
-          ) : (
-            <p>Loading...</p>
-          )}
+          <Image
+            src={imageUrl}
+            alt="Michael Donath"
+            className={styles.image}
+            layout="fill"
+          />
         </div>
         <h1 className={styles.title}>Lysius</h1>
       </div>
