8cbf63300269242d5496112a424a637421f5d8ab
Florian Schneider <schneider.f@me.com>
2024-07-25 13:53:24 +0200

    env

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,19 +1,23 @@
 export const fetchImages = async () => {
+  const apiUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL;
+  const apiToken = process.env.NEXT_PUBLIC_API_TOKEN;
+
+  console.log("API URL:", apiUrl);
+  console.log("API Token:", apiToken);
+
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/landingpage-images?populate=*`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
-        },
-      }
-    );
+    const res = await fetch(`${apiUrl}/api/landingpage-images?populate=*`, {
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+      },
+    });
 
     if (!res.ok) {
       throw new Error(`An error occurred: ${res.status}`);
     }
 
     const data = await res.json();
+    console.log("Fetched data:", data);
     return data.data.map((item) => ({
       url: item.attributes.Bilder.data.attributes.url,
       formats: item.attributes.Bilder.data.attributes.formats,
